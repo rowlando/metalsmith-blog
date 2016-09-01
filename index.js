d366982339653b64f77c0d285dbac97496c7f6fd
pco28 <pierre.coetzee@lad-tech.com>
2016-09-01 18:16:30 +0100

    Add collection to display list of posts

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 var Metalsmith  = require('metalsmith');
 var markdown    = require('metalsmith-markdown');
 var layouts     = require('metalsmith-layouts');
-// var permalinks  = require('metalsmith-permalinks');
+var collections = require('metalsmith-collections');
+var permalinks  = require('metalsmith-permalinks');
+var browserSync = require('metalsmith-browser-sync');
 
 Metalsmith(__dirname)
+  .use(browserSync({
+        files: ["src-markdown/**/*.md", "layouts/**/*.html"],
+        open: false
+  }))
+  .use(collections({
+    posts: {
+        pattern: 'posts/*.md',
+        sortBy: 'date',
+        reverse: true
+    }
+  }))
   .metadata({
     title: "My Static Site & Blog",
-    description: "It's about saying »Hello« to the World.",
+    description: "It is about saying to the ddd World!",
     generator: "Metalsmith",
     url: "http://www.metalsmith.io/"
   })
@@ -14,7 +27,7 @@ Metalsmith(__dirname)
   .destination('./build')
   .clean(false)
   .use(markdown())
-  // .use(permalinks())
+  .use(permalinks())
   .use(layouts({
     engine: 'handlebars'
   }))
---
+++ src-markdown/index.md
@@ -0,0 +1,4 @@
+---
+title: Rowlando's Blog  
+layout: layout.html
+---
